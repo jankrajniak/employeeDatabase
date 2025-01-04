9cbb6308495c9f604fa64273cbef0cab2b4c513d
jankrajniak <jan.krajniak@gmail.com>
2025-01-03 22:49:38 -0500

    querries used for data retrieval factored into retrieveData class in retrieveData.ts

--- src/index.ts
+++ src/index.ts
@@ -22,31 +22,17 @@
 import { QueryResult } from 'pg';
 
 // IMPORT CUSTOM PACKAGES
-import { pool, connectToDb } from './connections.js';
-import queries from './queries.js';
+import retrieveData from './retrieveData.js';
 
-await connectToDb();
 
-await pool.query(queries.allEmployees,  (err: Error, res: QueryResult) => {
-    if (err) {
-        console.log('Error retrieving employees data ', err);
-    } else {
-        console.log(res.rows);
-    }
-});
+const employees = await retrieveData.employees();
 
-await pool.query(queries.allDepartments, (err: Error, res: QueryResult) => {
-    if (err) {
-        console.log('Error retrieving deperatments data: ', err);
-    } else {
-        console.table(res.rows);
-    }
-});
+console.table(employees);
 
-await pool.query(queries.allRoles, (err: Error, res: QueryResult) => {
-    if (err) {
-        console.log('Error retrieving roles data: ', err);
-    } else {
-        console.table(res.rows);
-    }
-});
+const departments = await retrieveData.departments();
+
+console.table(departments);
+
+const roles = await retrieveData.roles();
+
+console.table(roles);
